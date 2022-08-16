e65698e53884f0af3edc3893310f52af3b53e1b7
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-08-16 17:04:58 +0900

    work page loadable

---
+++ pages/works/inkdrop.js
@@ -0,0 +1,23 @@
+import { Container, Badge, Link, List, ListItem } from '@chakra-ui/react'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, WorkImage, Meta } from '../../components/work'
+import Paragraph from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+    return(
+        <Layout title="Inkdrop">
+            <Container>
+                <Title>
+                    Inkdrop <Badge>2016</Badge>
+                </Title>
+                <Paragraph>
+                    A Markdown note-taking app with 111
+                </Paragraph>
+                <List ml={4} my={4}></List>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
