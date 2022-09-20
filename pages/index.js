19713d527b7b1d97f2973bcc6ee9c5c6d2a6e15e
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-09-19 21:53:23 -0400

    update on profile

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ const Page = () => {
             .
           </Paragraph>
           <Paragraph>
-            🤿 Recently, deep dive on data schema and Framer Motion.
+            🤿 Recently, deep dive on cryptography/blockchain and Framer Motion.
           </Paragraph>
           
           <Box align="center" my={4}>
