143d163f013a0cadcf69c430572fec515f29d2fc
ikang9712 <ikang9712@gmail.com>
2023-07-21 21:44:45 +0900

    feat: implement horror scene UI wireframe

--- src/pages/demo/bestHorror/index.tsx
+++ src/pages/demo/bestHorror/index.tsx
@@ -1,9 +1,34 @@
 import { NextPage } from 'next';
+import Image from 'next/image';
+import styled from 'styled-components';
+
+const Img = styled(Image)`
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+  border-radius: 15px;
+`;
+
+const PosterImage = ({
+  src,
+  width,
+  height,
+}: {
+  src: string;
+  width: number;
+  height: number;
+}) => {
+  return <Img src={src} alt="alt" width={width} height={height} />;
+};
+
+const ImageWrapper = styled.div`
+  & > span {
+    position: unset !important;
+  }
+`;
 
 const DemoBestHorror: NextPage = () => {
   return (
-    <div>
-      <header>
+    <div className="movieBody">
+      <header className="movieHeader">
         <h1>Best Horror Scenes</h1>
         <div>
           <h3>
@@ -29,8 +54,57 @@ const DemoBestHorror: NextPage = () => {
           </nav>
         </div>
       </header>
-      <main>
-        <section></section>
+      <main className="movieMain">
+        <section>
+          <div className="movieMainNav">
+            <div>
+              Currently viewing
+              <a className="" onClick={() => console.log('clicked!')}>
+                everything
+              </a>
+              sorted by
+              <a className="" onClick={() => console.log('clicked!')}>
+                random
+              </a>
+              .
+            </div>
+          </div>
+          <div className="movieMainWrapper">
+            <div className="movieMainHeader">
+              <h4>38. Hereditary (2018)</h4>
+              <h5>Directed by Me</h5>
+              <ImageWrapper>
+                <PosterImage
+                  src={'/images/demo/poster.png'}
+                  width={600}
+                  height={360}
+                />
+              </ImageWrapper>
+            </div>
+            <div className="movieMainHeader">
+              <h4>38. Hereditary (2018)</h4>
+              <h5>Directed by Me</h5>
+              <ImageWrapper>
+                <PosterImage
+                  src={'/images/demo/poster.png'}
+                  width={600}
+                  height={360}
+                />
+              </ImageWrapper>
+            </div>
+            <div className="movieMainHeader">
+              <h4>38. Hereditary (2018)</h4>
+              <h5>Directed by Me</h5>
+              <ImageWrapper>
+                <PosterImage
+                  src={'/images/demo/poster.png'}
+                  width={600}
+                  height={360}
+                />
+              </ImageWrapper>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
