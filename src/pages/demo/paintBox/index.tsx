a31a04214ed35d962fcddda19ba94b483e2917fa
ikang9712 <ikang9712@gmail.com>
2023-07-25 10:15:45 +0900

    feat: implement hero section

--- src/pages/demo/paintBox/index.tsx
+++ src/pages/demo/paintBox/index.tsx
@@ -64,13 +64,28 @@ const DemoPaintBox: NextPage = () => {
       {/* TODO: mobile menu */}
 
       <div className="home">
-        <h4>save 20% on bundles</h4>
-        <h3>
-          The ultimate summer accessory, discover our newest Nail Lackquer Trio:
-          <em>Like Poolside &ndash;</em>
-          inspired by color theory.
-        </h3>
-        <a href="#">shop now</a>
+        <div className="hero-section">
+          <div>
+            <Image
+              src={'/images/demo/paintbox/paintbox1.webp'}
+              alt="hero-image"
+              width={0}
+              height={0}
+              layout="responsive"
+            />
+          </div>
+
+          <div className="hero-text">
+            <h4>save 20% on bundles</h4>
+            <h3>
+              The ultimate summer accessory, discover our newest Nail Lackquer
+              Trio:
+              <em>Like Poolside &mdash;</em>
+              inspired by color theory.
+            </h3>
+            <a href="#">shop now</a>
+          </div>
+        </div>
       </div>
     </div>
   );
