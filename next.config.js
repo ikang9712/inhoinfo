4d12c3483e64d92f91e5697587800225c849a5d0
ikang9712 <ikang9712@gmail.com>
2023-07-28 15:41:40 +0900

    fix: trial#3 to fix vercel deployment bug

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const path = require('path');
+// const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    // prependData: `@import "src/styles/_variables.scss"; @import "src/styles/_mixins.scss";`,
-  },
 };
 
 module.exports = nextConfig;
